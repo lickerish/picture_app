cda311165bf194c8ae75bdbad7cd72d945a6c487
lickerish <kacper.lakomski@gmail.com>
2023-03-22 22:40:12 +0100

    Add grid

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,10 +1,16 @@
 <template>
   <div :class="[{ flexStart: step === 1 }, 'searchWrapper']">
+    <transition name="slide">
+      <LogoComponent v-if="step === 1" class="logo" />
+    </transition>
     <transition name="fade">
       <BackgroundComponent v-if="step === 0" />
     </transition>
     <ClaimComponent v-if="step === 0" />
     <SearchInputComponent :value="searchValue" v-model="searchValue" @input="handleInput" :dark="step === 1" />
+    <div class="results" v-if="results && !loading && step === 1">
+      <ItemComponent v-for="item in results" :item="item" :key="item.data[0].nasa_id" />
+    </div>
   </div>
 </template>
 
@@ -14,6 +20,8 @@ import SearchInputComponent from '@/components/SearchInputComponent.vue';
 import BackgroundComponent from '@/components/BackgroundComponent.vue';
 import debounce from 'lodash.debounce';
 import axios from 'axios';
+import LogoComponent from '@/components/LogoComponent.vue';
+import ItemComponent from '@/components/ItemComponent.vue';
 
 const API = 'https://images-api.nasa.gov/search';
 
@@ -23,6 +31,8 @@ export default {
     ClaimComponent,
     SearchInputComponent,
     BackgroundComponent,
+    LogoComponent,
+    ItemComponent,
   },
   data() {
     return {
@@ -39,6 +49,7 @@ export default {
       console.log(this.searchValue);
       this.loading = true;
       if (!this.searchValue) {
+        this.step = 0;
         return;
       }
       axios.get(`${API}?media_type=image&q=${this.searchValue}`).then((response) => {
@@ -70,11 +81,30 @@ export default {
 
 .fade-enter-active,
 .fade-leave-active {
-  transition: opacity .3s ease;
+  transition: opacity .5s ease;
 }
 
 .fade-enter,
 .fade-leave-to {
   opacity: 0;
+}
+
+.slide-enter-active {
+  transition: margin-top .5s ease;
+}
+
+.slide-enter {
+  margin-top: -50px;
+}
+
+.results {
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 20px;
+
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 }
 </style>
